1020a78db177412f96a2bd9bd72e792a04ff26cc
Oshinpou <oshinpounikar@gmail.com>
2025-05-16 19:18:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+  function viewAccount() { alert("Manage Account"); }
+  function viewCart() { alert("View Cart"); }
+  function viewOrders() { alert("Your Orders"); }
+  function trackOrders() { alert("Track Orders"); }
+</script>
